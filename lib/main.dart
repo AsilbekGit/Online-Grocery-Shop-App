ee9d98bba08a28d228a75dee18e55a5f352609f4
AsilbekGit <2110068@newuzbekistanuniversity.uz>
2023-12-08 11:48:09 +0500

    productDescriptionPage done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project/loginscreen.dart';
 import 'package:project/paymentPage.dart';
+import 'package:project/productDescriptionPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: PaymentPage(),
+      home: ProductDescriptionPage(),
     );
   }
 }
