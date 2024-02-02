5aa4d333565ee85b282f761927266c2dbc0f9d1b
stcoderts <115059135+stcoderts@users.noreply.github.com>
2024-02-02 16:00:08 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:project/pages/Sign_in.dart';
-import 'package:project/pages/home_page.dart';
-import 'package:project/pages/intro_screen.dart';
-import 'package:project/pages/paymentPage.dart';
-import 'package:project/pages/productDescriptionPage.dart';
-import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-import 'firebase_options.dart';
-import 'model/cart_model.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => CartModel(),
-      child: MyApp(),
-    ),
-  );
-}
-
 class MyApp extends StatelessWidget {
+  // ... (previous code)
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,7 +10,7 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: FutureBuilder(
-        future: checkSignInStatus(),
+        future: checkFirstSeen('intro'),  // Pass 'intro' for IntroScreen
         builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return CircularProgressIndicator();
@@ -44,27 +22,26 @@ class MyApp extends StatelessWidget {
           } else {
             // If the user is signed in, show the main page
             // Otherwise, show the appropriate screen based on whether the user has seen the app before
-            return snapshot.data ?? false ? HomePage() : IntroScreen();
+            if (snapshot.data ?? false) {
+              return HomePage();  // User is signed in
+            } else {
+              return IntroScreen();  // User is not signed in and IntroScreen is shown
+            }
           }
         },
       ),
     );
   }
 
-  Future<bool> checkSignInStatus() async {
+  Future<bool> checkFirstSeen(String page) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool _seen = (prefs.getBool('seen') ?? false);
-    bool _signedIn = (prefs.getBool('signedIn') ?? false);
+    bool _seen = (prefs.getBool('seen_$page') ?? false);
 
-    if (_signedIn) {
-      return true;
+    if (_seen) {
+      return false;
     } else {
-      if (_seen) {
-        return false;
-      } else {
-        await prefs.setBool('seen', true);
-        return true;
-      }
+      await prefs.setBool('seen_$page', true);
+      return true;
     }
   }
 }
